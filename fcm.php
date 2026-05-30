50ec817a42b1ff6d244873573182b50ad9144f5c
xNISTx <110129738+xNISTx@users.noreply.github.com>
2026-05-30 14:23:17 +0100

    Update fcm.php

--- fcm.php
+++ fcm.php
@@ -1,7 +1,3 @@
-$res = fcm_web_send(
-    ['dEhhz7kASsWM9fJkB4Fohh:APA91bEG87W1vNXXH3XXdSemRB28saQStQ84ylGzMZ0rt7yMqvEtwsRUWXVjgrXPIiY4emr9_YIrEZ0CIu-pufjLHeYPV6DXAcswis09uipR_ASLgX1h1Qc'],
-    'Test Notification',
-    'Hello from server'
-);
-
+<?php  
+$res = fcm_web_send(['dEhhz7kASsWM9fJkB4Fohh:APA91bEG87W1vNXXH3XXdSemRB28saQStQ84ylGzMZ0rt7yMqvEtwsRUWXVjgrXPIiY4emr9_YIrEZ0CIu-pufjLHeYPV6DXAcswis09uipR_ASLgX1h1Qc'], 'Test', 'Test');
 print_r($res);
