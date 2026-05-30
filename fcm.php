bb794d0812beb62d254cff30158e77b275df3539
xNISTx <110129738+xNISTx@users.noreply.github.com>
2026-05-30 12:14:00 +0100

    Add files via upload

---
+++ fcm.php
@@ -0,0 +1,7 @@
+$res = fcm_web_send(
+    ['dEhhz7kASsWM9fJkB4Fohh:APA91bEG87W1vNXXH3XXdSemRB28saQStQ84ylGzMZ0rt7yMqvEtwsRUWXVjgrXPIiY4emr9_YIrEZ0CIu-pufjLHeYPV6DXAcswis09uipR_ASLgX1h1Qc'],
+    'Test Notification',
+    'Hello from server'
+);
+
+print_r($res);
